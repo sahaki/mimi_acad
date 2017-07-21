6c970e9c19dc726fd8ccf6a26f17ff4a016c597f
sahaki <toners2print@gmail.com>
2017-07-21 18:16:29 +0700

    - Update setting form

---
+++ app/setting_general/general_tab.php
@@ -0,0 +1,49 @@
+<div class="panel panel-inverse" data-sortable-id="ui-modal-notification-1">
+    <div class="panel-heading">
+        <div class="panel-heading-btn">
+            <a href="javascript:;" class="btn btn-sm btn-icon btn-circle btn-warning" data-click="panel-expand" data-original-title="" title=""><i class="fa fa-expand"></i></a>
+        </div>
+        <h4 class="panel-title">ตั้งค่าข้อมูลเว็บไซต์</h4>
+    </div>
+    <div class="panel-body">
+        <form class="form-horizontal form-bordered" data-parsley-validate="true" name="demo-form" novalidate="">
+            <div class="form-group">
+                <legend>ข้อมูลทั่วไป</legend>
+                <label class="control-label col-md-3 col-sm-3" for="fullname">ชื่อระบบ :</label>
+                <div class="col-md-8 col-sm-8">
+                    <input class="form-control" type="text" id="fullname" name="fullname" placeholder="ชื่อระบบ" data-parsley-required="true" data-parsley-id="8558"><ul class="parsley-errors-list" id="parsley-id-8558"></ul>
+                </div>
+                <label class="control-label col-md-3 col-sm-3" for="fullname">หน่วยงาน :</label>
+                <div class="col-md-8 col-sm-8">
+                    <input class="form-control" type="text" id="fullname" name="fullname" placeholder="หน่วยงาน" data-parsley-required="true" data-parsley-id="8558"><ul class="parsley-errors-list" id="parsley-id-8558"></ul>
+                </div>
+                <label class="control-label col-md-3 col-sm-3" for="fullname">Logo :</label>
+                <div class="col-md-8 col-sm-8">
+                    <img src="../../media/person_register/images.jpg" width="50"><br>
+                    <div style="margin-top:5px;">คลิกที่รูปเพื่ออัพโหลดรูปใหม่</div>
+                </div>
+            </div>
+            <div class="form-group" style="margin-top: 10px;">
+                <legend>ฟอร์มบันทึกข้อมูล</legend>
+                <label class="control-label col-md-3 col-sm-3" for="fullname">ส่วนสูง :</label>
+                <div class="col-md-8 col-sm-8">
+                    <div class="col-md-2" style="margin: 0; padding: 0;">
+                        <input class="form-control" type="text" id="fullname" name="fullname" placeholder="ส่วนสูง" data-parsley-required="true" data-parsley-id="8558"><ul class="parsley-errors-list" id="parsley-id-8558"></ul>
+                    </div>
+                </div>
+
+                <label class="control-label col-md-3 col-sm-3" for="fullname">น้ำหนัก :</label>
+                <div class="col-md-8 col-sm-8">
+                    <div class="col-md-2" style="margin: 0; padding: 0;">
+                        <input class="form-control" type="text" id="fullname" name="fullname" placeholder="น้ำหนัก" data-parsley-required="true" data-parsley-id="8558"><ul class="parsley-errors-list" id="parsley-id-8558"></ul>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+        </form>
+    </div>
+</div>
